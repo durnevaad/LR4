1c61fa04de3c5d917df0e771068bba1fc5991690
durnevaad <93126385+durnevaad@users.noreply.github.com>
2021-11-06 16:36:29 +0500

    Добавление строки в таблицу (вариант 6)

---
+++ indexx.php
@@ -0,0 +1,34 @@
+<html>
+<head> <title> Сведения о холодильниках </title> </head>
+<body>
+<h2>Холодильники:</h2>
+<table border="1">
+		<tr>
+			<th> Номер</th>
+			<th> Марка</th>
+			<th> Модель</th>
+			<th> Внутренний объем</th>
+			<th> Редактировать</th>
+			<th> Удалить</th>
+		</tr>
+<?php	
+$link = mysqli_connect("127.0.0.1", "root", "", "fridge"); 
+ if (!$link) {
+    echo "Невозможно подключиться к серверу." ;
+}
+$result = mysqli_query($link, "SELECT id_f, brand, model, internal_volume FROM fridges");
+while ($row=mysqli_fetch_array($result)){
+ echo "<tr>";
+ echo "<td>" . $row['id_f'] . "</td>";
+ echo "<td>" . $row['brand'] . "</td>";
+ echo "<td>" . $row['model'] . "</td>";
+ echo "<td>" . $row['internal_volume'] . "</td>";
+ echo "<td><a href='edit.php?id=" . $row['id_user']. "'>Редактировать</a></td>"; 
+ echo "<td><a href='delete.php?id=" . $row['id_user']. "'>Удалить</a></td>"; 
+ echo "</tr>";}
+print "</table>";
+$num_rows = mysqli_num_rows($result); 
+print("<P>Всего моделей холодильников: $num_rows </p>");
+?>
+<p> <a href="neww.php"> Добавить модель холодильника </a>
+</body> </html>
