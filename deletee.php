50755d5024d365bf3658aa09b05cbd78e6b0ec30
durnevaad <93126385+durnevaad@users.noreply.github.com>
2021-11-06 17:01:26 +0500

    Удаление строки (вариант 6)

---
+++ deletee.php
@@ -0,0 +1,11 @@
+<?php
+$link = mysqli_connect("127.0.0.1", "root", "", "fridge"); 
+ if (!$link) {
+    echo "Невозможно подключиться к серверу." ;
+}
+ $id = $_GET['id'];
+ $sql = "DELETE FROM fridges WHERE id_f = '$id' ";
+ mysqli_query($link, $sql);
+ header("Location: indexx.php");
+ exit;
+?>
