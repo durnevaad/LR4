0877c7c1529a1579a04abd5023078c52b57268db
durnevaad <93126385+durnevaad@users.noreply.github.com>
2021-11-09 13:59:59 +0500

    Внесение корректировок

--- save_new.php
+++ save_new.php
@@ -3,17 +3,14 @@
  if (!$link) {
     echo "Невозможно подключиться к серверу." ;
 }
- $name = $_POST['name'];
- $login = $_POST['login'];
- $password = $_POST['password'];
- $e_mail = $_POST['e_mail'];
- $info = $_POST['info'];
- $sql = "INSERT INTO user (user_name, user_login, user_password, user_e_mail, user_info) 
- VALUES ('$name', '$login', '$password', '$e_mail', '$info')";
-if (mysqli_query($link, $sql)) {
-	print "<p>Все сохранено.";
- print "<p><a href=\"index.php\"> Вернуться к списку
-пользователей </a>"; }
-else { print "Ошибка сохранения. <a href=\"index.php\">
-Вернуться к списку книг </a>";}
+ $user_name = $_POST['user_name'];
+ $user_login = $_POST['user_login'];
+ $user_password = $_POST['user_password'];
+ $user_e_mail = $_POST['user_e_mail'];
+ $user_info = $_POST['user_info'];
+ $sql = "INSERT INTO user (user_name, user_login, user_password, user_e_mail, user_info) VALUES('$user_name','$user_login', '$user_password', '$user_e_mail', '$user_info')";
+  if (mysqli_query($link, $sql)) { 
+ print "<p>Все сохранено";
+ print "<p><a href=\"index.php\"> Вернуться к списку пользователей</a>"; }
+ else { print "Ошибка сохранения! <a href=\"index.php\"> Вернуться к списку пользователей </a>";}
 ?>
